26bbe0754f463e057d8a42310a8843351439d804
chaitanya1-stack <ck758779@gmail.com>
2025-06-27 01:13:53 +0530

    added about me section and added one project section made ui fully responsive

---
+++ src/components/animatedArrow.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../components/animatedArrow.css'; 
+
+const AnimatedArrow = () => {
+  return (
+    <div class="center-con">
+    
+    <div class="round">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+    </div>
+    
+</div>
+  );
+};
+
+export default AnimatedArrow;
--- src/components/dashBoard.jsx
+++ src/components/dashBoard.jsx
@@ -1,16 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef  } from 'react';
 import '../components/dashboard.css'; 
+import AnimatedArrow from './animatedArrow'; 
 import 'bootstrap-icons/font/bootstrap-icons.css';
-import { motion } from 'framer-motion';
-import { useInView } from 'react-intersection-observer';
+
+
 
 const Dashboard = () => {
-  // for animation on scrolling
-  const { ref: aboutRef, inView: aboutInView } = useInView({
-    triggerOnce: true,
-    threshold: 0.2,
-  });
-
   const [position, setPosition] = useState({ x: -100, y: -100 });
 
   useEffect(() => {
@@ -21,99 +16,117 @@ const Dashboard = () => {
     return () => window.removeEventListener('mousemove', handleMouseMove);
   }, []);
 
+//  for scrolling 
+  const aboutRef = useRef(null);
+  const projectsRef = useRef(null);
+  const resumeRef = useRef(null);
+
+const scrollToSection = (ref) => {
+  if (ref.current) {
+    ref.current.scrollIntoView({ behavior: 'smooth' });
+  }
+};
+
+
+
   return (
+
+
+    
     <div className="wholeContainer">
+
+       <div
+        className="cursor-glow"
+        style={{ left: `${position.x}px`, top: `${position.y}px` }}
+      ></div>
+
+
+
       <div className="navcontainer">
-        <nav className="custom-navbar">
-          {/* Left-aligned icons */}
+        <div className="custom-navbar">
+          <div className="name1">CHAITANYA KUMAR</div>
+
+
+
           <div className="icons d-flex">
             <i className="bi bi-github"></i>
             <i className="bi bi-linkedin"></i>
             <i className="bi bi-envelope"></i>
           </div>
 
-          {/* Right-aligned nav links */}
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <a className="nav-link" href="#about">About</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Projects</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Resume</a>
-            </li>
-          </ul>
-        </nav>
-      </div>
-
-      <div className="intro">
-        <div className="name">CHAITANYA KUMAR</div>
-      </div>
-
-      <div className="fullstack">
-        <div className="aspiring">Aspiring Full Stack Developer</div>
-      </div>
-
-      <div
-        className="cursor-glow"
-        style={{ left: `${position.x}px`, top: `${position.y}px` }}
-      ></div>
-
-      <div className="about" id="about" ref={aboutRef}>
-        <motion.div
-          className="left_column"
-          initial={{ opacity: 0, x: -50 }}
-          animate={aboutInView ? { opacity: 1, x: 0 } : {}}
-          transition={{ duration: 0.6 }}
-        >
-          <div className="aboutme">About Me</div>
-        </motion.div>
-
-        <div className="right_column">
-          {[
-            `Hi, I'm `,
-            `I'm currently building expertise in full-stack development using the MERN stack...`,
-            `I enjoy working on responsive UI design, creating RESTful APIs...`,
-            `Outside tech, I'm passionate about Photography...`,
-            `I'm currently looking for internship opportunities...`
-          ].map((text, index) => (
-            <motion.div
-              key={index}
-              className="aboutmedescription"
-              initial={{ opacity: 0, y: 40 }}
-              animate={aboutInView ? { opacity: 1, y: 0 } : {}}
-              transition={{ duration: 0.6, delay: 0.2 + index * 0.2 }}
-            >
-              {index === 0 && (
-                <>
-                  Hi, I'm <strong className="boldindescription">Chaitanya Kumar</strong>, a third-year B.Tech student at <strong className="boldindescription">IIT Guwahati</strong>, majoring in <strong className="boldindescription">Biosciences and Bioengineering</strong>. While my academic foundation lies in life sciences, my passion for <strong className="boldindescription">technology, web development, and problem-solving</strong> has led me to actively explore and build projects in the software domain.
-                </>
-              )}
-              {index === 1 && (
-                <>
-                  I’m currently building expertise in <strong className="boldindescription">full-stack development</strong> using the <strong className="boldindescription">MERN stack</strong>. Some of my key projects include <strong className="boldindescription">LearnDSA</strong>, a smart DSA practice tracker, and an <strong className="boldindescription">Online Compiler</strong> that lets users write, compile, and run code directly in the browser.
-                </>
-              )}
-              {index === 2 && (
-                <>
-                  I enjoy working on <strong className="boldindescription">responsive UI design</strong>, creating <strong className="boldindescription">RESTful APIs</strong>, and solving real-world problems through code. I also actively practice <strong className="boldindescription">DSA concepts</strong> to sharpen my logic and problem-solving skills.
-                </>
-              )}
-              {index === 3 && (
-                <>
-                  Outside tech, I'm passionate about <strong className="boldindescription">Photography</strong>, which adds a creative dimension to my work and perspective.
-                </>
-              )}
-              {index === 4 && (
-                <>
-                  I’m currently looking for <strong className="boldindescription">internship opportunities</strong>, where I can apply my skills, gain real-world experience, and contribute meaningfully as a developer.
-                </>
-              )}
-            </motion.div>
-          ))}
+         
         </div>
+
+
+      
       </div>
+
+      
+         <div className="intro_linksto">
+         <div className="intro">
+        <div className="introContent">
+        <div className="welcome">Welcome<strong className="dot">.</strong></div>
+        <div className="introval">
+           Hi, I'm <strong className="chaitanya">Chaitanya Kumar</strong> — a Full Stack Developer passionate about building seamless, responsive, and scalable web applications from frontend to backend.
+          </div>
+      </div>
+    </div>
+
+          <div className="linksto">
+            <div className="aboutmediv" onClick={() => scrollToSection(aboutRef)}><AnimatedArrow/><div className="aboutme">About me</div></div>
+             <div className="projectdiv" onClick={() => scrollToSection(projectsRef)} ><AnimatedArrow/><div className="project">Projects</div></div>
+             <div className="resumediv" onClick={() => scrollToSection(resumeRef)} ><AnimatedArrow/><div className="resume">Resume</div></div>
+            </div>
+
+
+        </div>
+
+
+        <div className="aboutmedescription_cont" ref={aboutRef}>
+          <div className="aboutmedescription">About me</div>
+          <div className="aboutmedescription_val">
+            <p>I'm currently a 3rd-year undergraduate student at <strong className="boldhighlight">IIT Guwahati</strong>, pursuing a major in <strong className="boldhighlight">Biosciences and Bioengineering</strong>. 
+               With a strong foundation in science and a passion for technology, I've been actively exploring the world of software development. 
+               Over time, I've developed into a <strong className="boldhighlight">Full Stack Developer</strong>, building responsive, scalable, and user-friendly web applications.</p>
+            <p>I'm particularly interested in blending my background in biosciences with digital solutions, and I'm constantly working on projects that improve usability, performance, and functionality. 
+                I enjoy working across the stack — from clean UI design to efficient backend logic and database management.</p>
+            <p>Outside of academics and coding, I'm always eager to learn new technologies, collaborate on impactful projects.</p>
+  
+ 
+          </div>
+        </div>
+
+
+        <div className="projectsdescription_cont" ref={projectsRef}>
+          <div className="projectsdescription">Projects</div>
+          <div className="projectsdescription_val">
+            <div className="tradegenix">TRADEGENIX–Stock Market Intelligence Dashboard <i class="bi bi-box-arrow-up-right"></i> <i class="bi bi-github"></i></div>
+            <div className="tradegenixdescription"> <p>TradeGeniX is a full-stack stock market dashboard designed for real-time financial analysis and visualization. It leverages third-party APIs like Finnhub to fetch live stock data, company overviews, and historical price charts—empowering users to make data-driven investment decisions.</p>
+            
+            <p className="keyfeatures">Key Features :</p>
+            <p> Real-time quotes & financial data via Finnhub API.</p>
+            <p> Search and track stocks (e.g., AAPL, TSLA).</p>
+            <p>5-year interactive charts (Recharts).</p>
+            <p> Modular component structure for scalability.</p>
+            <p> Scalable component architecture.</p>
+
+            <div className="techstack">
+              <div className="techstack1">React.js</div>
+              <div className="techstack1"> Recharts</div>
+              <div className="techstack1">Node.js</div>
+              <div className="techstack1">Express.js</div>
+              <div className="techstack1"> MongoDB</div>
+              <div className="techstack1"> REST API </div>
+              
+            </div>
+           
+       </div>
+          </div>
+        </div>
+     
+     
+
+      
     </div>
   );
 };
